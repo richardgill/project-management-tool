db29c88266634a0cfb457ee1502ee90b203e6e92
Richard Gill <richard@rgill.co.uk>
2021-09-30 18:45:13 +0100

    save up

--- packages/server/src/prototype/index.ts
+++ packages/server/src/prototype/index.ts
@@ -47,34 +47,74 @@ class TaskNode {
     }
   }
 
-  estimatedWorkdays(): number {
+  assignedEstimatedWorkdays(rejectStatuses: Status[]): number {
     return _.chain(this.children)
-      .map(n => n.estimatedWorkdays())
+      .map(n => n.assignedEstimatedWorkdays(rejectStatuses))
       .sum()
       .value()
   }
 
-  toObject(): Object {
+  unassignedEstimatedWorkdays(fallback: Resource, rejectStatuses: Status[]): number {
+    return _.chain(this.children)
+      .map(n => n.unassignedEstimatedWorkdays(fallback, rejectStatuses))
+      .sum()
+      .value()
+  }
+
+  totalEstimatedWorkdays(fallback: Resource, rejectStatuses: Status[]): number {
+    return this.assignedEstimatedWorkdays(rejectStatuses) + this.unassignedEstimatedWorkdays(fallback, rejectStatuses)
+  }
+
+  unfinishedChildren(): (TaskNode | Task)[] {
+    return this.children.filter(c => !c.isFinished())
+  }
+
+  taskCount(): number {
+    return _.chain(this.children)
+      .map(n => n.taskCount())
+      .sum()
+      .value()
+  }
+
+  unfinishedTaskCount(): number {
+    return _.chain(this.children)
+      .map(n => n.unfinishedTaskCount())
+      .sum()
+      .value()
+  }
+
+  isFinished(): boolean {
+    return _.every(this.children, c => c.isFinished())
+  }
+
+  calculate(fallback: Resource, remainingRejectStatuses: Status[]): Object {
     return {
       ...this,
-      children: this.children.map(c => c.toObject()),
+      children: this.children.map(c => c.calculate(fallback, remainingRejectStatuses)),
+      taskCount: this.taskCount(),
+      unfinishedTaskCount: this.unfinishedTaskCount(),
       sumOfEstimates: this.sumOfEstimates(),
-      estimatedWorkdays: this.estimatedWorkdays(),
+      assignedEstimatedWorkdays: this.assignedEstimatedWorkdays([]),
+      unassignedEstimatedWorkdays: this.unassignedEstimatedWorkdays(fallback, []),
+      totalEstimatedWorkdays: this.totalEstimatedWorkdays(fallback, []),
+      assignedRemainingEstimatedWorkdays: this.assignedEstimatedWorkdays(remainingRejectStatuses),
+      unassignedRemainingEstimatedWorkdays: this.unassignedEstimatedWorkdays(fallback, remainingRejectStatuses),
+      totalRemainingEstimatedWorkdays: this.totalEstimatedWorkdays(fallback, remainingRejectStatuses),
     }
   }
 }
 
 enum Status {
-  NOT_STARTED,
-  STARTED,
-  IN_REVIEW,
-  DONE,
+  NOT_STARTED = 'NOT_STARTED',
+  STARTED = 'STARTED',
+  IN_REVIEW = 'IN_REVIEW',
+  DONE = 'DONE',
 }
 
 enum EstimateUnit {
-  DAYS,
+  DAYS = 'DAYS',
   // HOURS,
-  STORY_POINTS,
+  STORY_POINTS = 'STORY_POINTS',
 }
 
 class Task {
@@ -94,21 +134,46 @@ class Task {
     this.description = description
   }
 
-  estimatedWorkdays() {
-    return this.estimateNumber / this.assignee.velocityMappingToCalendarWorkingDay[this.estimateUnit]
+  estimatedWorkdays(assignee: Resource, rejectStatuses: Status[]) {
+    if (!assignee || rejectStatuses.includes(this.status)) {
+      return 0
+    }
+    return this.estimateNumber / assignee.velocityMappingToCalendarWorkingDay[this.estimateUnit]
   }
 
-  sumOfEstimates() {
+  assignedEstimatedWorkdays(rejectStatuses: Status[]) {
+    return this.assignee ? this.estimatedWorkdays(this.assignee, rejectStatuses) : 0
+  }
+
+  unassignedEstimatedWorkdays(fallbackAssignee: Resource, rejectStatuses: Status[]) {
+    return this.assignee ? 0 : this.estimatedWorkdays(fallbackAssignee, rejectStatuses)
+  }
+
+  sumOfEstimates(rejectStatuses: Status[]) {
+    let estimateUnit in EstimateUnit
     return {
-      [EstimateUnit.DAYS]: this.estimateUnit === EstimateUnit.DAYS ? this.estimateNumber : 0,
-      [EstimateUnit.STORY_POINTS]: this.estimateUnit === EstimateUnit.STORY_POINTS ? this.estimateNumber : 0,
+      [EstimateUnit.DAYS.toString()]: this.estimateUnit === EstimateUnit.DAYS ? this.estimateNumber : 0,
+      [EstimateUnit.STORY_POINTS.toString()]: this.estimateUnit === EstimateUnit.STORY_POINTS ? this.estimateNumber : 0,
     }
   }
 
-  toObject(): Object {
+  isFinished(): boolean {
+    return this.status === Status.DONE
+  }
+
+  unfinishedTaskCount(): number {
+    return this.isFinished() ? 0 : 1
+  }
+
+  taskCount(): number {
+    return 1
+  }
+
+  calculate(fallback: Resource): Object {
     return {
       ...this,
-      estimatedWorkdays: this.estimatedWorkdays(),
+      assignedEstimatedWorkdays: this.assignedEstimatedWorkdays([]),
+      unassignedEstimatedWorkdays: this.unassignedEstimatedWorkdays(fallback, []),
     }
   }
 }
@@ -120,19 +185,28 @@ class Task {
 // calendar days missing
 // no prioritization (what order do we tackle work)
 
+const averageResource = (resources: Resource[]) => {
+  // todo implement properly
+  return resources[0]
+}
+
 console.log('starting prototype')
 
 const eoin = new User('enugent', 'Eoin')
 const yaw = new Resource('Yaw', { [EstimateUnit.DAYS]: 0.4, [EstimateUnit.STORY_POINTS]: 1 })
 const richard = new Resource('Richard', { [EstimateUnit.DAYS]: 0.8, [EstimateUnit.STORY_POINTS]: 2 })
 
+const fallbackResource = averageResource([yaw, richard])
+
 const connectDocs = new Task('Ungate `klarna_payments`', Status.NOT_STARTED, 2, EstimateUnit.DAYS, yaw, 'Klarna Payments')
-const paymentIntentDocs = new Task('PI Docs', Status.NOT_STARTED, 2, EstimateUnit.DAYS, richard, 'Klarna Payments')
+const paymentIntentDocs = new Task('PI Docs', Status.IN_REVIEW, 2, EstimateUnit.DAYS, richard, 'Klarna Payments')
 const finishTheDocs = new TaskNode('Complete docs', eoin, [connectDocs, paymentIntentDocs], 'Get Klarna docs ready for GA')
 
 const checkoutDogfooding = new Task('Checkout Dogfooding', Status.NOT_STARTED, 2, EstimateUnit.DAYS, richard, 'Checkout Dogfooding')
-const piDogfooding = new Task('Payment Intents Dogfooding', Status.NOT_STARTED, 2, EstimateUnit.STORY_POINTS, richard, 'PI Dogfooding')
-const dogfooding = new TaskNode('Dogfooding', eoin, [checkoutDogfooding, piDogfooding], 'Dogfooding Klarna')
+const piDogfooding = new Task('Payment Intents Dogfooding', Status.DONE, 2, EstimateUnit.STORY_POINTS, richard, 'PI Dogfooding')
+const upeDogfooding = new Task('UPE Dogfooding', Status.NOT_STARTED, 2, EstimateUnit.STORY_POINTS)
+const dogfooding = new TaskNode('Dogfooding', eoin, [checkoutDogfooding, piDogfooding, upeDogfooding], 'Dogfooding Klarna')
+
 const root = new TaskNode('Klarna GA', eoin, [finishTheDocs, dogfooding], 'Get Klarna to GA')
 
-console.log(JSON.stringify(root.toObject(), null, 2))
+console.log(JSON.stringify(root.calculate(fallbackResource, [Status.DONE]), null, 2))
